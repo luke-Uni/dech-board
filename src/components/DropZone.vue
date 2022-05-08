2f3fcc7dad3931f5f03193200d56205296c55d83
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-05-08 21:39:10 +0200

    commit on master

---
+++ src/components/DropZone.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    @dragenter.prevent="toggleActive"
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{ 'active-dropzone': active }"
+    class="dropzone"
+  >
+    <span>Drag or Drop File</span>
+    <span>OR</span>
+    <label for="dropzoneFile">Select File</label>
+    <input type="file" id="dropzoneFile" class="dropzoneFile" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "DropZone",
+  setup() {
+    const active = ref(false);
+    const toggleActive = () => {
+      active.value = !active.value;
+    };
+    return { active, toggleActive };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.dropzone {
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 16px;
+  border: 2px dashed #56d8ff;
+  background-color: #fff;
+  transition: 0.3s ease all;
+  label {
+    padding: 8px 12px;
+    color: #fff;
+    background-color: #56d8ff;
+    transition: 0.3s ease all;
+  }
+  input {
+    display: none;
+  }
+}
+.active-dropzone {
+  color: #fff;
+  border-color: #fff;
+  background-color: #56d8ff;
+  label {
+    background-color: #fff;
+    color: #56d8ff;
+  }
+}
+</style>
