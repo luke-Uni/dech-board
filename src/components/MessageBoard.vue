57aa82c5a603d2c4398e5f85b149c044038be416
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-05-08 17:32:24 +0200

    MessageBoard with DummieText

--- src/components/MessageBoard.vue
+++ src/components/MessageBoard.vue
@@ -7,19 +7,33 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       title: "Title",
+      text: "That is the text",
       upVotes: 7,
       downVotes: 2,
     };
+  },
+  methods: {
+    async getAllPosts() {
+      let headers = {};
+
+      let uri = "";
+
+      let res = await axios.get(uri, { headers: headers });
+
+      console.log(headers);
+    },
   },
 };
 </script>
 
 <style scoped>
 .posts {
+  align-content: center;
   border: 3px solid black;
   width: 20%;
   height: 10%;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
 
 <script>
 // @ is an alias to /src
-import MessageBoard from "../components/MessageBoard.vue";
+import MessageBoard from "@/components/MessageBoard.vue";
 
 export default {
   components: {
