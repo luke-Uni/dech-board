fb993cb764a162f360dcf4b6805247a8d80890e3
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-05-08 22:53:07 +0200

    messageBoard Date added

--- src/components/MessageBoard.vue
+++ src/components/MessageBoard.vue
@@ -4,6 +4,9 @@
       <h2 class="title">{{ post.title }}</h2>
       <h4>{{ post.username }}</h4>
       <p>{{ post.text }}</p>
+      <p>
+        <b> {{ post.creationDate }} </b>
+      </p>
     </div>
   </div>
 </template>
@@ -39,6 +42,7 @@ export default {
           text: "Online class is so boring. I want to have class at the university and want to see my fellow student.     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
           upVotes: 12,
           downVotes: 4,
+          creationDate: "2022-05-06T20:14:22.511Z",
         },
         {
           title: "The Weather",
@@ -49,8 +53,9 @@ export default {
         },
       ];
 
-      this.posts[0] = allPosts[0];
-      this.posts[1] = allPosts[1];
+      for (let index = 0; index < allPosts.length; index++) {
+        this.posts[index] = allPosts[index];
+      }
     },
   },
 };
