e828d1f0f816a36f3529f615b0719982623a4160
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-05-08 13:02:12 +0200

    first div for post

---
+++ src/components/MessageBoard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="posts"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.posts {
+  width: 20%;
+  height: 10%;
+}
+</style>
