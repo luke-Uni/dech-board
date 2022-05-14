d79fc8071cc677ae549d8cc32bfc917cc36f1416
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-05-14 18:04:18 +0200

    design

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/createpost" >Create a Post</router-link>|
-    <router-link to="/registeruser" >Register</router-link>|
+    <router-link to="/createpost" >Create a Post</router-link> |
+    <router-link to="/registeruser" >Register</router-link> |
     <router-link to="/login" >Login</router-link>
   </nav>
   <router-view />
