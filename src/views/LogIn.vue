25cfc97b896c5a17972303b66ae2a20d91161408
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-05-17 16:13:41 +0200

    Importing files

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,63 +1,29 @@
-
 <template>
-  <!-- <input
-        type="text"
-        class="input-username"
-        placeholder="Username"
-        v-model="username"
-      />
-
-<br>
-      <input
-        type="text"
-        class="input-title"
-        placeholder="Password"
-        v-model="password"
-      />
-<br> -->
-
-  <div class="top"></div>
+  <div class="top">
+  </div>
   <div class="container">
     <form class="form">
       <fieldset>
-        <!-- <img src="../assets/1.png" alt="HeNan" id="logo" /> -->
+        <img src="../assets/1.png" alt="HeNan" id="logo">
         <label for="username"></label>
-        <input
-          type="text"
-          id="username"
-          placeholder=" &nbsp;&nbsp;Username"
-          v-model="username"
-        />
+        <input type="text" id="username" placeholder=" &nbsp;&nbsp;Username" v-model="username" />
         <br />
         <label for="password"></label>
-        <input
-          type="password"
-          id="password"
-          placeholder=" &nbsp;&nbsp;Password"
-          v-model="password"
-        />
+        <input type="password" id="password" placeholder=" &nbsp;&nbsp;Password" v-model="password" />
         <br />
-        <button class="primary" @click="loginUser()" id="loginButton">
-          Login
-        </button>
+        <button class="primary" @click="loginUser()" id="loginButton">Log In</button>
         <a href="#" id="fotgotten">Forgotten password</a>
         <div class="bottom"></div>
-        <button
-          class="primary"
-          @click="goToRegister($event)"
-          id="registerButton"
-        >
-          Register
-        </button>
+        <button class="primary" @click="goToRegister($event)" id="registerButton">Create new account</button>
       </fieldset>
     </form>
   </div>
   <div class="footer">
     <div id="leftImg">
-      <!-- <img src="../assets/河南师范大学.png" alt="" /> -->
+      <img src="../assets/河南师范大学.png" alt="">
     </div>
     <div id="rightImg">
-      <!-- <img src="../assets/logo.svg" alt="" /> -->
+      <img src="../assets/logo.svg" alt="">
     </div>
   </div>
 </template>
@@ -66,8 +32,6 @@
 import axios from "axios";
 
 export default {
-  name: "LogIn",
-
   data() {
     return {
       username: "",
@@ -90,7 +54,7 @@ export default {
 
           this.tokens[0] = result.data;
           localStorage.setItem("token", this.tokens[0].token);
-          localStorage.setItem("username", this.username);
+         // localStorage.setItem("username", this.username);
         });
 
       //     console.log(result.data);
@@ -98,12 +62,17 @@ export default {
       //    // console.log(result.Object.data);
       //     console.log(result.constructor.data);
       console.log(result);
+      this.$router.push("/");
     },
+
+
     checkPassword(password) {
       return this.users.filter((user) => password === user.password);
     },
+
     checkCredentials(event) {
       event.preventDefault();
+
       if (
         this.username === this.currentUser &&
         this.checkPassword(this.password).length !== 0
@@ -118,105 +87,143 @@ export default {
     },
     goToRegister(event) {
       event.preventDefault();
-      this.$router.push("register");
+      this.$router.push("registeruser");
     },
   },
 };
 </script>
 
 <style scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
 #logo {
-  margin-bottom: 30px;
+  margin-bottom: 10%;
+  width: 80%;
+  margin-left: 10%;
 }
-#leftImg {
-  /* background-color: black; */
-  height: 40px;
-  width: 20%;
-  float: left;
-}
-#leftImg img {
-  width: 65%;
-  opacity: 0.7;
-}
-#rightImg img {
-  width: 65%;
-}
+
 #rightImg {
   /* background-color: blue; */
-  height: 40px;
-  width: 20%;
+  /* height: 80%; */
+  width: 10%;
+  float: right;
+  background-color: white;
+}
+
+#leftImg {
+  /* background-color: black; */
+  /* height: 80%; */
+  width: 10%;
+  float: left;
+  background-color: white;
+}
+
+#leftImg img {
+  width: 100%;
+  /* opacity: 0.7; */
+}
+
+#rightImg img {
+  width: 80%;
   float: right;
 }
-.top,
+
+.top,.footer{
+  /* top: 0; */
+  background-color: rgba(146, 196, 224, 255);
+  box-sizing: border-box;
+  /* position: absolute; */
+  width: 100%;
+  height: 80px;
+}
+.top{
+  /* position: absolute; */
+  top: 0;
+}
 .footer {
-  height: 60px;
-  background-color: aqua;
+
 }
+
 .container {
+  
   border: 1px solid black;
-  width: 400px;
-  height: 500px;
-  /* background-color: aqua; */
+  width: 23%;
   border-radius: 60px;
-  margin: 3% auto;
+  margin: 2% auto;
 }
+
 .form {
-  /* padding: 40px 35px; */
   display: block;
-  margin: 40px auto;
+  margin: 10% auto;
 }
+
 #username {
   border-radius: 20px;
-  margin-bottom: 40px;
-  width: 300px;
+  margin-bottom: 10%;
+  width: 80%;
   height: 40px;
-  margin-left: 35px;
-  box-shadow: 3px 3px 3px grey;
+  margin-left: 10%;
+  box-shadow: 0px 0px 5px grey;
+  border: 0 grey solid;
 }
+
 #password {
   border-radius: 20px;
   margin-bottom: 40px;
-  width: 300px;
+  width: 80%;
   height: 40px;
-  margin-left: 35px;
-  box-shadow: 3px 3px 3px grey;
+  margin-left: 10%;
+  box-shadow: 0px 0px 5px grey;
+  border: 0 grey solid;
 }
+
 #loginButton {
   border-radius: 20px;
-  margin-bottom: 20px;
-  width: 300px;
+  margin-bottom: 10%;
+  width: 80%;
   height: 40px;
-  background-color: cadetblue;
-  margin-left: 35px;
+  background-color: rgba(146, 196, 224, 255);
+  border: 0ch white solid;
+  margin-left: 10%;
+  box-shadow: 0px 0px 5px rgba(146, 196, 224, 255);
+  color: white;
 }
+
 #registerButton {
   border-radius: 20px;
-  margin-bottom: 40px;
-  width: 200px;
+  /* margin-bottom: 40px; */
+  width: 60%;
   height: 40px;
-  background-color: rgb(63, 218, 63);
-  margin-left: 75px;
+  background-color: rgba(30, 203, 58, 255);
+  margin-left: 20%;
+  color: white;
+  font-size: 60%;
+  border: 0ch white solid;
+  box-shadow: 0px 0px 5px rgba(30, 203, 58, 255);
 }
+
 #fotgotten {
-  margin-left: 90px;
-  margin-bottom: 20px;
+  display: block;
+  margin-left: 30%;
+  /* margin-bottom: 20px; */
+  width: 40%;
+  font-size: 30%;
+  text-align: center;
 }
+
 input {
   margin: 0 auto;
 }
-img {
-  width: 300px;
-  height: 50px;
-  margin-bottom: 5px;
-  margin-left: 35px;
-}
+
 .bottom {
   /* background-color: black; */
-  width: 300px;
+  width: 80%;
   height: 2px;
-  margin-left: 35px;
-  margin-top: 20px;
+  margin-left: 10%;
+  margin-top: 10%;
   border-top: 2px solid black;
-  margin-bottom: 20px;
+  margin-bottom: 10%;
 }
 </style>
