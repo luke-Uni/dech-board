85551f2f936aa5285c3c4214cd56982b60217e21
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-05-19 19:33:30 +0200

    update style login

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,29 +1,39 @@
 <template>
-  <div class="top">
-  </div>
+  <div class="top"></div>
   <div class="container">
     <form class="form">
-      <fieldset>
-        <img src="../assets/1.png" alt="HeNan" id="logo">
-        <label for="username"></label>
-        <input type="text" id="username" placeholder=" &nbsp;&nbsp;Username" v-model="username" />
-        <br />
-        <label for="password"></label>
-        <input type="password" id="password" placeholder=" &nbsp;&nbsp;Password" v-model="password" />
-        <br />
-        <button class="primary" @click="loginUser()" id="loginButton">Log In</button>
-        <a href="#" id="fotgotten">Forgotten password</a>
-        <div class="bottom"></div>
-        <button class="primary" @click="goToRegister($event)" id="registerButton">Create new account</button>
-      </fieldset>
+      <img src="../assets/1.png" alt="HeNan" id="logo" />
+      <label for="username"></label>
+      <input
+        type="text"
+        id="username"
+        placeholder=" &nbsp;&nbsp;Username"
+        v-model="username"
+      />
+      <br />
+      <label for="password"></label>
+      <input
+        type="password"
+        id="password"
+        placeholder=" &nbsp;&nbsp;Password"
+        v-model="password"
+      />
+      <br />
+      <button class="primary" @click="loginUser()" id="loginButton">
+        Log In
+      </button>
+      <div class="bottom"></div>
+      <button class="primary" @click="goToRegister($event)" id="registerButton">
+        Create new account
+      </button>
     </form>
   </div>
   <div class="footer">
     <div id="leftImg">
-      <img src="../assets/河南师范大学.png" alt="">
+      <img src="../assets/河南师范大学.png" alt="" />
     </div>
     <div id="rightImg">
-      <img src="../assets/logo.svg" alt="">
+      <img src="../assets/logo.svg" alt="" />
     </div>
   </div>
 </template>
@@ -36,7 +46,7 @@ export default {
     return {
       username: "",
       password: "",
-      currentUser: "flo",
+      currentUser: "",
       feedback: "",
       tokens: [],
     };
@@ -54,7 +64,7 @@ export default {
 
           this.tokens[0] = result.data;
           localStorage.setItem("token", this.tokens[0].token);
-         // localStorage.setItem("username", this.username);
+          // localStorage.setItem("username", this.username);
         });
 
       //     console.log(result.data);
@@ -64,7 +74,6 @@ export default {
       console.log(result);
       this.$router.push("/");
     },
-
 
     checkPassword(password) {
       return this.users.filter((user) => password === user.password);
@@ -94,7 +103,7 @@ export default {
 </script>
 
 <style scoped>
-*{
+* {
   margin: 0;
   padding: 0;
 }
@@ -130,7 +139,8 @@ export default {
   float: right;
 }
 
-.top,.footer{
+.top,
+.footer {
   /* top: 0; */
   background-color: rgba(146, 196, 224, 255);
   box-sizing: border-box;
@@ -138,16 +148,12 @@ export default {
   width: 100%;
   height: 80px;
 }
-.top{
+.top {
   /* position: absolute; */
   top: 0;
 }
-.footer {
-
-}
 
 .container {
-  
   border: 1px solid black;
   width: 23%;
   border-radius: 60px;
@@ -164,7 +170,6 @@ export default {
   margin-bottom: 10%;
   width: 80%;
   height: 40px;
-  margin-left: 10%;
   box-shadow: 0px 0px 5px grey;
   border: 0 grey solid;
 }
@@ -174,7 +179,6 @@ export default {
   margin-bottom: 40px;
   width: 80%;
   height: 40px;
-  margin-left: 10%;
   box-shadow: 0px 0px 5px grey;
   border: 0 grey solid;
 }
@@ -186,7 +190,6 @@ export default {
   height: 40px;
   background-color: rgba(146, 196, 224, 255);
   border: 0ch white solid;
-  margin-left: 10%;
   box-shadow: 0px 0px 5px rgba(146, 196, 224, 255);
   color: white;
 }
@@ -197,20 +200,9 @@ export default {
   width: 60%;
   height: 40px;
   background-color: rgba(30, 203, 58, 255);
-  margin-left: 20%;
   color: white;
-  font-size: 60%;
   border: 0ch white solid;
   box-shadow: 0px 0px 5px rgba(30, 203, 58, 255);
-}
-
-#fotgotten {
-  display: block;
-  margin-left: 30%;
-  /* margin-bottom: 20px; */
-  width: 40%;
-  font-size: 30%;
-  text-align: center;
 }
 
 input {
