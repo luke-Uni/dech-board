8efdd06e68f6fe1d58378fac0a35a40938a693a3
arvandh439 <arvandhashemi@gmail.com>
2022-05-24 00:05:17 +0200

    test

---
+++ src/components/alluserList.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="see Users">
+      
+      <button
+        type="userList"
+        class=""
+        @click="userList()"
+      >
+        <span class="icon-cross"></span>
+        <span class="visually-hidden"></span>
+      </button>
+    </div>
+
+</template>
+
+
+
+
+<script>
+
+ //just testing(copy of the component createPost)
+import axios from "axios";
+
+export default {
+  name: "getUsers",
+
+  props: ["TogglePopup"],
+
+  data() {
+    return {
+    //  username: "",
+    //  title: "",
+    //  content: "",
+    //  important: "",
+    };
+  },
+
+  methods: {
+    async getUsers() {
+      let result = await axios.post(
+        //"https://dech-board-rest-server.herokuapp.com/posts/create",
+        "http://localhost:8090/posts/getUsers",
+        {
+          //username: this.username,
+          users:this.getUsers,
+          //title: this.title,
+         // content: this.content,
+         // important: this.important,
+        },
+        {
+          //headers: {
+          //  authorization: localStorage.getItem("token"),
+         // },
+        }
+      );
+
+      console.log(result);
+    },
+  },
+};
+</script>
