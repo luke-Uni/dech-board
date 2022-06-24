a21228f4cc4d97f253711ef4b9a84440ac39c786
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-06-24 14:06:12 +0200

    Style popup

--- src/components/MessageBoardCreator.vue
+++ src/components/MessageBoardCreator.vue
@@ -13,11 +13,16 @@
         </button>
         <br />
         <div class="MessageBoardName">
-          <input
+          <!-- <input
             type="text"
             v-model="MessageBoardName"
             placeholder="New Messageboard"
-          />
+          /> -->
+        <div class="form__group field">
+  <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
+  <label for="name" class="form__label">MessageBoard</label>
+</div>
+<br>
         </div>
         <div class="ChooseUser">
           <v-container fluid>
@@ -172,6 +177,94 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+$primary: #838383;
+$secondary: #b9b9b9;
+$white: #fff;
+$gray: #8d8d8d;
+.form__group {
+  position: relative;
+  padding: 15px 0 0;
+  margin-top: 10px;
+  width: 50%;
+}
+
+.form__field {
+  font-family: inherit;
+  width: 100%;
+  border: 0;
+  border-bottom: 2px solid $gray;
+  outline: 0;
+  font-size: 1.3rem;
+  color: gray;
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+
+  &::placeholder {
+    color: transparent;
+  }
+
+  &:placeholder-shown ~ .form__label {
+    font-size: 1.3rem;
+    cursor: text;
+    top: 20px;
+  }
+}
+
+.form__label {
+  position: absolute;
+  top: 0;
+  display: block;
+  transition: 0.2s;
+  font-size: 1rem;
+  color: $gray;
+}
+
+.form__field:focus {
+  ~ .form__label {
+    position: absolute;
+    top: 0;
+    display: block;
+    transition: 0.2s;
+    font-size: 1rem;
+    color: $primary;
+    font-weight:700;    
+  }
+  padding-bottom: 6px;  
+  font-weight: 700;
+  border-width: 3px;
+  border-image: linear-gradient(to right, $primary,$secondary);
+  border-image-slice: 1;
+}
+/* reset input */
+.form__field{
+  &:required,&:invalid { box-shadow:none; }
+}
+/* demo */
+body {
+  font-family: 'Poppins', sans-serif; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  font-size: 1.5rem;
+  background-color:#222222;
+}
+body {
+  font-family: 'Poppins', sans-serif; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  font-size: 1.5rem;
+  background-color:#222222;
+}
+
+
+
 // .fade-enter-active,
 // .fade-leave-active{
 
@@ -317,7 +410,7 @@ export default {
 }
 
 .message-create {
-  width: 40em;
+  width: 21em;
 
   padding: 0.5em;
   margin: auto;
@@ -349,11 +442,12 @@ export default {
 .send-button-div {
   width: 22%;
   height: 100%;
-  float: right;
-  padding-right: 1em;
+  
+  // padding-right: 1em;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-left: 9em;
 }
 /* CSS */
 .button-81 {
