0e74d7c817587c2f59693e4d85347eed1213658d
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-06-26 15:00:54 +0200

    Translation API

--- src/components/MessageBoard.vue
+++ src/components/MessageBoard.vue
@@ -92,55 +92,35 @@ export default {
     localStorage.setItem("conversationID", "");
   },
   methods: {
-    //Translate Text to English
-    async translateTextToEnglish(text) {
-      let hallo1 = "";
+    async translateTextToGerman(text){
 
-      const axios = require("axios");
+  let hallo1 = "";        
+      let url="http://localhost:8090/translatePost";
+      let result = await axios.post( url,{
+        text: text.content,
+        source:"auto",
+        target:"de"
+      },
+      {
+          // headers: {
+          //   authorization: localStorage.getItem("token"),
+          //   "Content-Type": undefined,
+          // },
+        }
 
-      const encodedParams = new URLSearchParams();
-      encodedParams.append("q", text.content);
-      encodedParams.append("target", "en");
-      encodedParams.append("source", "en");
-
-      const options = {
-        method: "POST",
-        url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
-          "X-RapidAPI-Key":
-            // "7d1b34c23cmshf82fb063e5c4987p15f36fjsn2ae75f209a89",
-            // "5d1d5e4d29msh38383cbe012e65dp1bc8ecjsnc81fe54408d0",
-            //"702f1e51f7msh51a1f9495e6f4e0p10180bjsnba5626672929",
-            //"21ec3285f4msh32c045448fe36e4p1c3201jsna2215e8d5989",
-            //'6d5d589836mshca9bfd68f2db605p1818f9jsn5dec50884640',
-
-            //1. Account
-            //"4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08",
-            //2. Account
-            //'e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd'
-            //'4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08'
-            //2. Account
-            "e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd",
-          //3. Account
-          //'9147a39e0emsh2d31c0b791076f8p1f7faejsnbbf307c7865f'
-        },
-        data: encodedParams,
-      };
-
-      await axios
-        .request(options)
-        .then(function (response) {
-          hallo1 = response.data.data.translations[0].translatedText;
+      )
+      .then(function (result) {
+       // console.log("-------------------------"+response.data.data);
+       // console.log("-------------------------"+response.data.data.TargetText);
+      //  console.log("-------------------------"+response.data.data.targeText);
+          hallo1 = result.data.TargetText;
           console.log(hallo1);
           console.log(typeof hallo1);
         })
         .catch(function (error) {
           console.error(error);
         });
-
-      for (let i = 0; i < this.posts.length; i++) {
+    for (let i = 0; i < this.posts.length; i++) {
         console.log(this.posts[i].postId + " " + text.postId);
         console.log("1 TranslationStatus: " + this.posts[i].translationStatus);
         if (this.posts[i].postId == text.postId) {
@@ -165,151 +145,285 @@ export default {
           }
         }
       }
+
+   console.log(result);
     },
+
+    async translateTextToChinese(text){
+
+  let hallo1 = "";        
+      let url="http://localhost:8090/translatePost";
+      let result = await axios.post( url,{
+        text: text.content,
+        source:"auto",
+        target:"zh"
+      },
+      {
+          // headers: {
+          //   authorization: localStorage.getItem("token"),
+          //   "Content-Type": undefined,
+          // },
+        }
+
+      )
+      .then(function (result) {
+       // console.log("-------------------------"+response.data.data);
+       // console.log("-------------------------"+response.data.data.TargetText);
+      //  console.log("-------------------------"+response.data.data.targeText);
+          hallo1 = result.data.TargetText;
+          console.log(hallo1);
+          console.log(typeof hallo1);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    for (let i = 0; i < this.posts.length; i++) {
+        console.log(this.posts[i].postId + " " + text.postId);
+        console.log("1 TranslationStatus: " + this.posts[i].translationStatus);
+        if (this.posts[i].postId == text.postId) {
+          console.log(
+            "2 TranslationStatus: " + this.posts[i].translationStatus
+          );
+          if (this.posts[i].translationStatus == false) {
+            console.log(
+              "3 TranslationStatus: " + this.posts[i].translationStatus
+            );
+            //if(  this.posts[i].content != hallo1){
+            //  this.posts[i].originalcontent =  this.posts[i].content;
+            // }
+            //this.posts[i].originalcontent =  this.posts[i].content;
+            this.posts[i].translatedcontent = hallo1;
+            this.posts[i].translationStatus = true;
+          } else {
+            console.log(
+              " 4TranslationStatus: " + this.posts[i].translationStatus
+            );
+            this.posts[i].translationStatus = false;
+          }
+        }
+      }
+
+   console.log(result);
+    },
+
+    // //Translate Text to English
+    // async translateTextToEnglish(text) {
+    //   let hallo1 = "";
+
+    //   const axios = require("axios");
+
+    //   const encodedParams = new URLSearchParams();
+    //   encodedParams.append("q", text.content);
+    //   encodedParams.append("target", "en");
+    //   encodedParams.append("source", "en");
+
+    //   const options = {
+    //     method: "POST",
+    //     url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    //     headers: {
+    //       "content-type": "application/x-www-form-urlencoded",
+    //       "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    //       "X-RapidAPI-Key":
+    //         // "7d1b34c23cmshf82fb063e5c4987p15f36fjsn2ae75f209a89",
+    //         // "5d1d5e4d29msh38383cbe012e65dp1bc8ecjsnc81fe54408d0",
+    //         //"702f1e51f7msh51a1f9495e6f4e0p10180bjsnba5626672929",
+    //         //"21ec3285f4msh32c045448fe36e4p1c3201jsna2215e8d5989",
+    //         //'6d5d589836mshca9bfd68f2db605p1818f9jsn5dec50884640',
+
+    //         //1. Account
+    //         //"4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08",
+    //         //2. Account
+    //         //'e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd'
+    //         //'4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08'
+    //         //2. Account
+    //         "e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd",
+    //       //3. Account
+    //       //'9147a39e0emsh2d31c0b791076f8p1f7faejsnbbf307c7865f'
+    //     },
+    //     data: encodedParams,
+    //   };
+
+    //   await axios
+    //     .request(options)
+    //     .then(function (response) {
+    //       hallo1 = response.data.data.translations[0].translatedText;
+    //       console.log(hallo1);
+    //       console.log(typeof hallo1);
+    //     })
+    //     .catch(function (error) {
+    //       console.error(error);
+    //     });
+
+    //   for (let i = 0; i < this.posts.length; i++) {
+    //     console.log(this.posts[i].postId + " " + text.postId);
+    //     console.log("1 TranslationStatus: " + this.posts[i].translationStatus);
+    //     if (this.posts[i].postId == text.postId) {
+    //       console.log(
+    //         "2 TranslationStatus: " + this.posts[i].translationStatus
+    //       );
+    //       if (this.posts[i].translationStatus == false) {
+    //         console.log(
+    //           "3 TranslationStatus: " + this.posts[i].translationStatus
+    //         );
+    //         //if(  this.posts[i].content != hallo1){
+    //         //  this.posts[i].originalcontent =  this.posts[i].content;
+    //         // }
+    //         //this.posts[i].originalcontent =  this.posts[i].content;
+    //         this.posts[i].translatedcontent = hallo1;
+    //         this.posts[i].translationStatus = true;
+    //       } else {
+    //         console.log(
+    //           " 4TranslationStatus: " + this.posts[i].translationStatus
+    //         );
+    //         this.posts[i].translationStatus = false;
+    //       }
+    //     }
+    //   }
+    // },
 
     //Translate Text into German
-    async translateTextToGerman(text) {
-      let hallo1 = "";
+    // async translateTextToGerman(text) {
+    //   let hallo1 = "";
 
-      const axios = require("axios");
+    //   const axios = require("axios");
 
-      const encodedParams = new URLSearchParams();
-      encodedParams.append("q", text.content);
-      encodedParams.append("target", "de");
-      encodedParams.append("source", "en");
+    //   const encodedParams = new URLSearchParams();
+    //   encodedParams.append("q", text.content);
+    //   encodedParams.append("target", "de");
+    //   encodedParams.append("source", "en");
 
-      const options = {
-        method: "POST",
-        url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
-          "X-RapidAPI-Key":
-            // "7d1b34c23cmshf82fb063e5c4987p15f36fjsn2ae75f209a89",
-            // "5d1d5e4d29msh38383cbe012e65dp1bc8ecjsnc81fe54408d0",
-            //"702f1e51f7msh51a1f9495e6f4e0p10180bjsnba5626672929",
-            //"21ec3285f4msh32c045448fe36e4p1c3201jsna2215e8d5989",
-            //'6d5d589836mshca9bfd68f2db605p1818f9jsn5dec50884640',
+    //   const options = {
+    //     method: "POST",
+    //     url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    //     headers: {
+    //       "content-type": "application/x-www-form-urlencoded",
+    //       "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    //       "X-RapidAPI-Key":
+    //         // "7d1b34c23cmshf82fb063e5c4987p15f36fjsn2ae75f209a89",
+    //         // "5d1d5e4d29msh38383cbe012e65dp1bc8ecjsnc81fe54408d0",
+    //         //"702f1e51f7msh51a1f9495e6f4e0p10180bjsnba5626672929",
+    //         //"21ec3285f4msh32c045448fe36e4p1c3201jsna2215e8d5989",
+    //         //'6d5d589836mshca9bfd68f2db605p1818f9jsn5dec50884640',
 
-            //1. Account
-            //'4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08'
-            //2. Account
-            "e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd",
-          //3. Account
-          //'9147a39e0emsh2d31c0b791076f8p1f7faejsnbbf307c7865f'
-        },
-        data: encodedParams,
-      };
+    //         //1. Account
+    //         //'4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08'
+    //         //2. Account
+    //         "e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd",
+    //       //3. Account
+    //       //'9147a39e0emsh2d31c0b791076f8p1f7faejsnbbf307c7865f'
+    //     },
+    //     data: encodedParams,
+    //   };
 
-      await axios
-        .request(options)
-        .then(function (response) {
-          hallo1 = response.data.data.translations[0].translatedText;
-          console.log(hallo1);
-          console.log(typeof hallo1);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+    //   await axios
+    //     .request(options)
+    //     .then(function (response) {
+    //       hallo1 = response.data.data.translations[0].translatedText;
+    //       console.log(hallo1);
+    //       console.log(typeof hallo1);
+    //     })
+    //     .catch(function (error) {
+    //       console.error(error);
+    //     });
 
-      for (let i = 0; i < this.posts.length; i++) {
-        console.log(this.posts[i].postId + " " + text.postId);
-        console.log("1 TranslationStatus: " + this.posts[i].translationStatus);
-        if (this.posts[i].postId == text.postId) {
-          console.log(
-            "2 TranslationStatus: " + this.posts[i].translationStatus
-          );
-          if (this.posts[i].translationStatus == false) {
-            console.log(
-              "3 TranslationStatus: " + this.posts[i].translationStatus
-            );
-            // if(  this.posts[i].content != hallo1){
-            //   this.posts[i].originalcontent =  this.posts[i].content;
-            // }
-            //this.posts[i].originalcontent =  this.posts[i].content;
-            this.posts[i].translatedcontent = hallo1;
-            this.posts[i].translationStatus = true;
-          } else {
-            console.log(
-              " 4TranslationStatus: " + this.posts[i].translationStatus
-            );
-            this.posts[i].translationStatus = false;
-          }
-        }
-      }
-    },
+    //   for (let i = 0; i < this.posts.length; i++) {
+    //     console.log(this.posts[i].postId + " " + text.postId);
+    //     console.log("1 TranslationStatus: " + this.posts[i].translationStatus);
+    //     if (this.posts[i].postId == text.postId) {
+    //       console.log(
+    //         "2 TranslationStatus: " + this.posts[i].translationStatus
+    //       );
+    //       if (this.posts[i].translationStatus == false) {
+    //         console.log(
+    //           "3 TranslationStatus: " + this.posts[i].translationStatus
+    //         );
+    //         // if(  this.posts[i].content != hallo1){
+    //         //   this.posts[i].originalcontent =  this.posts[i].content;
+    //         // }
+    //         //this.posts[i].originalcontent =  this.posts[i].content;
+    //         this.posts[i].translatedcontent = hallo1;
+    //         this.posts[i].translationStatus = true;
+    //       } else {
+    //         console.log(
+    //           " 4TranslationStatus: " + this.posts[i].translationStatus
+    //         );
+    //         this.posts[i].translationStatus = false;
+    //       }
+    //     }
+    //   }
+    // },
 
     //Translate into Chinese
-    async translateTextToChinese(text) {
-      let hallo1 = "";
+    // async translateTextToChinese(text) {
+    //   let hallo1 = "";
 
-      const axios = require("axios");
+    //   const axios = require("axios");
 
-      const encodedParams = new URLSearchParams();
-      encodedParams.append("q", text.originalcontent);
-      encodedParams.append("target", "zh-CN");
-      encodedParams.append("source", "en");
+    //   const encodedParams = new URLSearchParams();
+    //   encodedParams.append("q", text.originalcontent);
+    //   encodedParams.append("target", "zh-CN");
+    //   encodedParams.append("source", "en");
 
-      const options = {
-        method: "POST",
-        url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
-          "X-RapidAPI-Key":
-            // "7d1b34c23cmshf82fb063e5c4987p15f36fjsn2ae75f209a89",
-            // "5d1d5e4d29msh38383cbe012e65dp1bc8ecjsnc81fe54408d0",
-            //"702f1e51f7msh51a1f9495e6f4e0p10180bjsnba5626672929",
-            //"21ec3285f4msh32c045448fe36e4p1c3201jsna2215e8d5989",
-            //'6d5d589836mshca9bfd68f2db605p1818f9jsn5dec50884640',
+    //   const options = {
+    //     method: "POST",
+    //     url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    //     headers: {
+    //       "content-type": "application/x-www-form-urlencoded",
+    //       "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    //       "X-RapidAPI-Key":
+    //         // "7d1b34c23cmshf82fb063e5c4987p15f36fjsn2ae75f209a89",
+    //         // "5d1d5e4d29msh38383cbe012e65dp1bc8ecjsnc81fe54408d0",
+    //         //"702f1e51f7msh51a1f9495e6f4e0p10180bjsnba5626672929",
+    //         //"21ec3285f4msh32c045448fe36e4p1c3201jsna2215e8d5989",
+    //         //'6d5d589836mshca9bfd68f2db605p1818f9jsn5dec50884640',
 
-            //1. Account
-            //'4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08'
-            //2. Account
-            "e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd",
-          //3. Account
-          //'9147a39e0emsh2d31c0b791076f8p1f7faejsnbbf307c7865f'
-        },
-        data: encodedParams,
-      };
+    //         //1. Account
+    //         //'4371de4373msh1114eee19dec596p11b9fcjsn3422372a9f08'
+    //         //2. Account
+    //         "e77a4f3011mshdde71ca27045aa4p15281fjsn9290da687ebd",
+    //       //3. Account
+    //       //'9147a39e0emsh2d31c0b791076f8p1f7faejsnbbf307c7865f'
+    //     },
+    //     data: encodedParams,
+    //   };
 
-      await axios
-        .request(options)
-        .then(function (response) {
-          hallo1 = response.data.data.translations[0].translatedText;
-          console.log(hallo1);
-          console.log(typeof hallo1);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+    //   await axios
+    //     .request(options)
+    //     .then(function (response) {
+    //       hallo1 = response.data.data.translations[0].translatedText;
+    //       console.log(hallo1);
+    //       console.log(typeof hallo1);
+    //     })
+    //     .catch(function (error) {
+    //       console.error(error);
+    //     });
 
-      for (let i = 0; i < this.posts.length; i++) {
-        console.log(this.posts[i].postId + " " + text.postId);
-        console.log("1 TranslationStatus: " + this.posts[i].translationStatus);
-        if (this.posts[i].postId == text.postId) {
-          console.log(
-            "2 TranslationStatus: " + this.posts[i].translationStatus
-          );
-          if (this.posts[i].translationStatus == false) {
-            console.log(
-              "3 TranslationStatus: " + this.posts[i].translationStatus
-            );
-            // if(  this.posts[i].content != hallo1){
-            //   this.posts[i].originalcontent =  this.posts[i].content;
-            // }
-            //this.posts[i].originalcontent =  this.posts[i].content;
-            this.posts[i].translatedcontent = hallo1;
-            this.posts[i].translationStatus = true;
-          } else {
-            console.log(
-              " 4TranslationStatus: " + this.posts[i].translationStatus
-            );
-            this.posts[i].translationStatus = false;
-          }
-        }
-      }
-    },
+    //   for (let i = 0; i < this.posts.length; i++) {
+    //     console.log(this.posts[i].postId + " " + text.postId);
+    //     console.log("1 TranslationStatus: " + this.posts[i].translationStatus);
+    //     if (this.posts[i].postId == text.postId) {
+    //       console.log(
+    //         "2 TranslationStatus: " + this.posts[i].translationStatus
+    //       );
+    //       if (this.posts[i].translationStatus == false) {
+    //         console.log(
+    //           "3 TranslationStatus: " + this.posts[i].translationStatus
+    //         );
+    //         // if(  this.posts[i].content != hallo1){
+    //         //   this.posts[i].originalcontent =  this.posts[i].content;
+    //         // }
+    //         //this.posts[i].originalcontent =  this.posts[i].content;
+    //         this.posts[i].translatedcontent = hallo1;
+    //         this.posts[i].translationStatus = true;
+    //       } else {
+    //         console.log(
+    //           " 4TranslationStatus: " + this.posts[i].translationStatus
+    //         );
+    //         this.posts[i].translationStatus = false;
+    //       }
+    //     }
+    //   }
+    // },
     //To display all the Posts we need to get them from the Server
     async getAllPosts(boardId) {
       console.log("I am in the getAllPosts function");
